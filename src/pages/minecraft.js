e6e20f314733d2f895db13607bfda7f5aefe8a90
Gavin Papaleo <gavin@Jordans-iMac.local>
2019-08-02 08:36:14 -0700

    Added picturs of end portal

--- src/pages/minecraft.js
+++ src/pages/minecraft.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Minecraft = () => (
+const Minecraft = (props) => (
   <Layout>
     <SEO title="Minecraft" />
     <h1>Minecraft</h1>
@@ -31,8 +32,52 @@ const Minecraft = () => (
       
     </ol>
 
+    <h4>How to find the ender portal</h4>
+    <ol>
+      <li>
+        <p>Use  your eye of ender and it will fly away from you into the air befor dropping back to the ground. Follow it, pick it up, and repeat. There's a 1 in 5 chance that the eye will shatter when it drops, witch is why you need several. </p>
+      </li>
+      <li>
+        <p>Eventuall the eye wiil keep falling onto the same spot of land. This is were you'll find the stronghold, and you'll need to dig underground to get in. Just remember the number one rule for Minecraft: Never dig straight dawn, or you could fall into the lava in the center of the portal.</p>
+      </li>
+      <li>
+        <p></p>
+      </li>
+      <li>
+        <p></p>
+      </li>     
+    </ol>
+
+
+    <Img fluid={props.data.portalRoom.childImageSharp.fluid} />
+    <Img fluid={props.data.endPortal.childImageSharp.fluid} />
+
+
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
 export default Minecraft
+
+
+export const fixedImage = graphql`
+  fragment fixedImage on File {
+    childImageSharp {
+      fixed(height: 700) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+`
+
+export const pageQuery = graphql`
+  query {
+    portalRoom: file(relativePath: { eq: "StrongholdPortalRoom.png" }) {
+      ...fluidImage
+    }
+    endPortal: file(relativePath: { eq: "End_Portal.png" }) {
+      ...fluidImage
+    }
+  }
+`
